796f23db0d6ed890dbc9c3be8a26f972018952c9
Romain Rivollier <romain.rivollier@gmail.com>
2015-04-26 12:02:23 +0200

    add dynamic framework project

---
+++ Source/WhereAmI.h
@@ -0,0 +1,19 @@
+//
+//  WhereAmI.h
+//  WhereAmI
+//
+//  Created by Romain Rivollier on 26/04/15.
+//  Copyright (c) 2015 Romain Rivollier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhereAmI.
+FOUNDATION_EXPORT double WhereAmIVersionNumber;
+
+//! Project version string for WhereAmI.
+FOUNDATION_EXPORT const unsigned char WhereAmIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhereAmI/PublicHeader.h>
+
+
